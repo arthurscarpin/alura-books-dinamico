addacf9e90bdcbc6be440e583fc965600ec09d7d
arthurscarpin <scarpinarthur.dev@gmail.com>
2024-11-05 22:35:30 -0300

    feat: adicionando metodos reduce e sort

--- app/metodoForEach.js
+++ app/metodoForEach.js
@@ -1,11 +1,14 @@
 const elementosParaInserirLivros = document.getElementById('livros');
+const elementoComValorTotalDeLivrosDisponiveis = document.getElementById('valor_total_livros_disponiveis');
 
 function exibirOsLivrosNaTela(listaDeLivros) {
+    elementoComValorTotalDeLivrosDisponiveis.innerHTML = '';
     elementosParaInserirLivros.innerHTML = '';
     listaDeLivros.forEach(livro => {
+        let disponibilidade = livro.quantidade > 0 ? 'livro__imagens' : 'livro__imagens indisponivel';
         elementosParaInserirLivros.innerHTML += `       
         <div class="livro">
-            <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
+            <img class="${disponibilidade}" src="${livro.imagem}" alt="${livro.alt}" />
             <h2 class="livro__titulo">${livro.titulo}</h2>
             <p class="livro__descricao">${livro.autor}</p>
             <p class="livro__preco" id="preco">R$${livro.preco.toFixed(2)}</p>
